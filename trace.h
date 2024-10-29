01a309a662eb2513c05115f52f8123d7baab9ddf
Tchatchesamuel <samueltchatche1@gmail.com>
2024-10-29 08:10:49 +0100

    Update

---
+++ trace.h
@@ -0,0 +1,10 @@
+#include <iostream>
+// procedure pour calculer la trace d'une matrice carre M2x2
+int trac2(int ma[2][2]) {
+    return ma[0][0]+ma[1][1] ;
+}
+// procedure pour calculer la trace d'une matruce carre M3×3
+	int trac3(int ma[3][3]){
+		return ma[0][0]+ma[1][1]+ma[2][2];
+		}
+	
